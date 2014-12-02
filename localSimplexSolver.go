b1c91604f87955003bef812555b5726b3fc5f4d6
Neal Borkert <neal.borkert@gmail.com>
2014-12-01 20:45:53 -0500

    skeleton works, now need to work on innards

--- localSimplexSolver.go
+++ localSimplexSolver.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"solver"
 	"time"
-	"runtime"
 )
 
 func main() {
@@ -20,23 +19,22 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-
+	var listOfPlayers []solver.Player
 	for scanner.Scan() {
 		newLine := scanner.Text()
 //		fmt.Println(newLine)
 		player := solver.CreatePlayer(newLine)
 //		fmt.Printf("Created player %v\n", player)
-		solver.AddPlayerToSingleList(player)
+		listOfPlayers = solver.AddPlayerToSingleList(player)
 	}
-//	fmt.Println(allPlayers)
+	fmt.Println(listOfPlayers)
 	startTime := time.Now()
 	winningRoster := solver.CreateSimplexRoster()
 	elapsed := time.Since(startTime)
 //	winningPoints := solver.PointsForRoster(winningRoster)
-/*
+
 	fmt.Printf("Winning roster is %v\n", winningRoster)
-	fmt.Printf("Winning points total is %v\n", winningPoints)
-	fmt.Printf("Winning roster salary is %v\n", solver.RosterSalary(winningRoster))
-	*/
+//	fmt.Printf("Winning points total is %v\n", winningPoints)
+//	fmt.Printf("Winning roster salary is %v\n", solver.RosterSalary(winningRoster))
 	fmt.Printf("Time required to find winning roster = %v\n", elapsed)
 }
