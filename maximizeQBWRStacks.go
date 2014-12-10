dbeec623e85b830486945d037675e54dbaef7b2a
Neal Borkert <neal.borkert@gmail.com>
2014-12-10 14:55:50 -0500

    create stacks by points, too

--- maximizeQBWRStacks.go
+++ maximizeQBWRStacks.go
@@ -12,6 +12,7 @@ import (
 type QBWRStack struct {
 	PlayerNames string
 	Team        string
+	ProjectedPoints float64
 	Value       float64
 }
 
@@ -26,6 +27,20 @@ func (this ByVal) Less(i, j int) bool {
 }
 
 func (this ByVal) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+type ByPoints []QBWRStack
+
+func (this ByPoints) Len() int {
+	return len(this)
+}
+
+func (this ByPoints) Less(i, j int) bool {
+	return this[i].ProjectedPoints > this[j].ProjectedPoints
+}
+
+func (this ByPoints) Swap(i, j int) {
 	this[i], this[j] = this[j], this[i]
 }
 
@@ -55,7 +70,7 @@ func main() {
 		if player.Position == "QB" {
 			for _, WR := range allPlayers {
 				if WR.Position == "WR" && player.Team == WR.Team {
-					stack := QBWRStack{player.PlayerName + " + " + WR.PlayerName, player.Team, (player.ProjectedPoints + WR.ProjectedPoints) / float64(player.Salary+WR.Salary)}
+					stack := QBWRStack{player.PlayerName + " + " + WR.PlayerName, player.Team, player.ProjectedPoints + WR.ProjectedPoints, (player.ProjectedPoints + WR.ProjectedPoints) / float64(player.Salary+WR.Salary)}
 					//fmt.Println(stack)
 					stacks = append(stacks, stack)
 				}
@@ -64,8 +79,15 @@ func main() {
 	}
 
 	sort.Sort(ByVal(stacks))
-
+	fmt.Println("Stacks by value:")
 	for _, stack := range stacks {
-		fmt.Println(stack.PlayerNames + " , value = " + strconv.FormatFloat(stack.Value, 'f', 6, 64))
+		fmt.Println(stack.PlayerNames + ", value = " + strconv.FormatFloat(stack.Value, 'f', 6, 64))
+	}
+
+	sort.Sort(ByPoints(stacks))
+	fmt.Println("*************************")
+	fmt.Println("Stacks by projected points:")
+	for _, stack := range stacks {
+		fmt.Println(stack.PlayerNames +", projected points = "+strconv.FormatFloat(stack.ProjectedPoints, 'f', 6, 64))
 	}
 }
