25faac068dde89d6b6f31649978ba1556718223b
Neal Borkert <neal.borkert@gmail.com>
2014-12-10 15:27:36 -0500

    moved functions to solver package

--- maximizeQBWRStacks.go
+++ maximizeQBWRStacks.go
@@ -9,41 +9,6 @@ import (
 	"strconv"
 )
 
-type PlayerStack struct {
-	PlayerNames     string
-	Team            string
-	ProjectedPoints float64
-	Value           float64
-}
-
-type ByVal []PlayerStack
-
-func (this ByVal) Len() int {
-	return len(this)
-}
-
-func (this ByVal) Less(i, j int) bool {
-	return this[i].Value > this[j].Value
-}
-
-func (this ByVal) Swap(i, j int) {
-	this[i], this[j] = this[j], this[i]
-}
-
-type ByPoints []PlayerStack
-
-func (this ByPoints) Len() int {
-	return len(this)
-}
-
-func (this ByPoints) Less(i, j int) bool {
-	return this[i].ProjectedPoints > this[j].ProjectedPoints
-}
-
-func (this ByPoints) Swap(i, j int) {
-	this[i], this[j] = this[j], this[i]
-}
-
 func main() {
 	file, err := os.Open(os.Args[1])
 
@@ -64,13 +29,13 @@ func main() {
 
 	//Build stacks. Loop over array of players, picking QBs,
 	//then relooping and finding WRs on same team
-	stacks := make([]PlayerStack, 0)
+	stacks := make([]solver.PlayerStack, 0)
 	for _, player := range allPlayers {
 		//fmt.Println("Player = ", player)
 		if player.Position == "QB" {
 			for _, WR := range allPlayers {
 				if WR.Position == "WR" && player.Team == WR.Team {
-					stack := PlayerStack{player.PlayerName + " + " + WR.PlayerName, player.Team, player.ProjectedPoints + WR.ProjectedPoints, (player.ProjectedPoints + WR.ProjectedPoints) / float64(player.Salary+WR.Salary)}
+					stack := solver.PlayerStack{player.PlayerName + " + " + WR.PlayerName, player.Team, player.ProjectedPoints + WR.ProjectedPoints, (player.ProjectedPoints + WR.ProjectedPoints) / float64(player.Salary+WR.Salary)}
 					//fmt.Println(stack)
 					stacks = append(stacks, stack)
 				}
@@ -78,13 +43,13 @@ func main() {
 		}
 	}
 
-	sort.Sort(ByVal(stacks))
+	sort.Sort(solver.ByVal(stacks))
 	fmt.Println("Stacks by value:")
 	for _, stack := range stacks {
 		fmt.Println(stack.PlayerNames + ", value = " + strconv.FormatFloat(stack.Value, 'f', 6, 64))
 	}
 
-	sort.Sort(ByPoints(stacks))
+	sort.Sort(solver.ByPoints(stacks))
 	fmt.Println("*************************")
 	fmt.Println("Stacks by projected points:")
 	for _, stack := range stacks {
